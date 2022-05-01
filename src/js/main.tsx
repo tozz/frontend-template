d1db7470382d5c7a635954e403ec94ba0a231957
Erik Johansson <github@tozz.org>
2022-05-01 14:03:00 +0200

    Update dependencies, including React 18

--- src/js/main.tsx
+++ src/js/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Component } from './component';
 
 document.addEventListener('readystatechange', () => {
   if (document.readyState === 'complete') {
-    ReactDOM.render(<Component />, document.getElementById('root'));
+    const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement);
+    root.render(
+      <React.StrictMode>
+        <Component />
+      </React.StrictMode>
+    );
   }
 });
