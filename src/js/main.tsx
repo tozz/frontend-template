f96803fcd808ed0e7f9f57563dd1e48be7350865
Erik Johansson <erik@arkkapital.com>
2023-02-10 20:24:18 +0100

    Better index.html defaults and tailwind prettier plugin

--- src/js/main.tsx
+++ src/js/main.tsx
@@ -4,7 +4,7 @@ import { Component } from './component';
 
 document.addEventListener('readystatechange', () => {
   if (document.readyState === 'complete') {
-    const root = createRoot(document.getElementById('root') as HTMLDivElement);
+    const root = createRoot(document.getElementById('app') as HTMLDivElement);
     root.render(
       <StrictMode>
         <Component />
